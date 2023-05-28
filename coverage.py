88025b6910f08e9a9ae2e7d2062a954fed1b9af2
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2023-05-28 11:07:05 +0900

    Add script file that checks diary's coverage

---
+++ coverage.py
@@ -0,0 +1,32 @@
+from datetime import date, timedelta
+from os import path
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def main():
+    d_start = date(2022, 8, 11)
+    d_end = date.today()
+
+    count_total = 0
+    count_exist = 0
+    missing_diaries = []
+
+    for d in daterange(d_start, d_end + timedelta(days=1)):
+        count_total += 1
+
+        d_path = d.strftime("%Y/%m/%d.md")
+        if path.isfile(d_path):
+            count_exist += 1
+        else:
+            missing_diaries.append(d_path)
+
+    print("Coverage: {} / {}".format(count_exist, count_total))
+
+    if len(missing_diaries)  > 0:
+        print("Missing :")
+        for m in missing_diaries:
+            print(" " + m)
+
+main()
